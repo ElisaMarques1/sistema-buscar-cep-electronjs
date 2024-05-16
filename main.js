a19e43a5ebabff1ce09bda2f30ef42867c43b7fd
elisa marques <elisamarques466@gmail.com>
2024-05-15 21:52:16 -0300

    Criando a interface do Projeto

---
+++ main.js
@@ -0,0 +1,24 @@
+//importando modulos do electron
+const { app, BrowserWindow } = require('electron')
+
+const path = require('path')
+
+require('electron-reload')(__dirname, {
+  electron: path.join(__dirname, 'node_modules', '.bin', 'electron')
+});
+
+
+//função que cria a janela do electron
+const createWindow = () => {
+    const win = new BrowserWindow({
+      width: 800,
+      height: 600
+    })
+  
+    win.loadFile('index.html')
+  }
+
+//rodando a aplicação
+  app.whenReady().then(() => {
+    createWindow()
+  })
